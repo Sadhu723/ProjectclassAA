f9a229111021c6bb240ee6ce7b8f36e50e28f99b
sadhu723 <hussainksatham628@gmail.com>
2022-08-23 17:42:48 +0530

    first time push

---
+++ src/main/java/com/gitclassnew/Classnew.java
@@ -0,0 +1,20 @@
+package com.gitclassnew;
+
+public class Classnew {
+
+	private void mathod1() {
+		  System.out.println("1");
+
+		}
+		
+		private void mathod2() {
+			System.out.println("2");
+
+		}
+		
+		private void mathod3() {
+			System.out.println("3");
+
+		}
+
+}
